1d33e1aa48387e38728fa8bbc03be2879cf4efb6
Mani <manikandanvenkatachalam@Mac.modem>
2026-04-27 18:13:04 +1000

    string init

--- app/src/main/java/com/example/newsapp/ui/screens/SavedScreen.kt
+++ app/src/main/java/com/example/newsapp/ui/screens/SavedScreen.kt
@@ -12,6 +12,8 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import com.example.newsapp.R
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
 import androidx.navigation.NavController
@@ -55,7 +57,7 @@ fun SavedContent(
         CommonSearchBar(
             query = searchQuery,
             onQueryChange = onSearchQueryChange,
-            placeholder = "Search saved articles..."
+            placeholder = stringResource(R.string.SaveScreen_searchPlaceholder)
         )
 
         Box(
